7b4d7d52ae89e7610157399c9accf33e7e8a2121
increano <increano@gmail.com>
2025-05-18 09:00:59 +0200

    Refactor: Update Webpack Cache and Manifests
    - Updated app-build-manifest.json for improved build performance.
    - Refreshed client and server webpack cache files to ensure up-to-date bundling.
    - Adjusted server and app paths manifests for better routing accuracy.
    - Enhanced server-reference-manifest.json for optimized server-side rendering.
    - Updated webpack-runtime.js to incorporate latest runtime changes.
    - General maintenance and cleanup of cache and trace files.

--- packages/minimal-ui-next/.next/server/webpack-runtime.js
+++ packages/minimal-ui-next/.next/server/webpack-runtime.js
@@ -130,7 +130,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("fcad4a2535d97779")
+/******/ 		__webpack_require__.h = () => ("84563db62f60c41f")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
